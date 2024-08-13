3ff61438d4cb76e7c98b17b8f060a0f4ae9b382f
Yauheni Kadyrau <7077634@gmail.com>
2024-08-13 15:19:08 +0300

    feat:  config server-json

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server')
+
+const server = jsonServer.create()
+
+const router = jsonServer.router('db.json')
+
+server.use(router)
+server.listen(3002, () => {
+    console.log('JSON Server is running')
+})
+
+module.exports = server
