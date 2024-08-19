fbac0b949d5ce548023bfc3367afba7cc3bb3c67
Yauheni Kadyrau <7077634@gmail.com>
2024-08-19 13:28:21 +0300

    refactor:  removed comments; removed icons into public/icons; messages into public/locale

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,15 +3,13 @@ import { getRequestConfig } from "next-intl/server";
 import { locales } from "config";
 
 export default getRequestConfig(async ({ locale }) => {
-    // Validate that the incoming `locale` parameter is valid
     if (!locales.includes(locale as any)) notFound();
 
     return {
         messages: (
             await (locale === "en"
-                ? // When using Turbopack, this will enable HMR for `en`
-                  import("../messages/en.json")
-                : import(`../messages/${locale}.json`))
+                ? import("../public/locales/en.json")
+                : import(`../public/locales/${locale}.json`))
         ).default,
     };
 });
